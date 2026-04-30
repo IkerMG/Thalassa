9ed6fda1cb7027d119fa333fa23c4f5a409a3ad7
IkerMG <ikermozoga@gmail.com>
2026-04-30 21:28:01 +0200

    feat(frontend): extender uiStore con historial de mensajes del chat (6A)

--- frontend/src/store/uiStore.ts
+++ frontend/src/store/uiStore.ts
@@ -1,19 +1,26 @@
 import { create } from 'zustand';
+import type { ChatMessage } from '../types/chat';
 
 interface UIState {
   isChatOpen: boolean;
   isSidebarCollapsed: boolean;
+  messages: ChatMessage[];
   openChat: () => void;
   closeChat: () => void;
   toggleChat: () => void;
   toggleSidebar: () => void;
+  appendMessage: (msg: ChatMessage) => void;
+  clearMessages: () => void;
 }
 
 export const useUIStore = create<UIState>((set) => ({
   isChatOpen: false,
   isSidebarCollapsed: false,
+  messages: [],
   openChat: () => set({ isChatOpen: true }),
   closeChat: () => set({ isChatOpen: false }),
   toggleChat: () => set((s) => ({ isChatOpen: !s.isChatOpen })),
   toggleSidebar: () => set((s) => ({ isSidebarCollapsed: !s.isSidebarCollapsed })),
+  appendMessage: (msg) => set((s) => ({ messages: [...s.messages, msg] })),
+  clearMessages: () => set({ messages: [] }),
 }));
